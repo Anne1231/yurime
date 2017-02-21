b561ce68f26488f7f0e52d160bc46029db6e2d54
Anne1231 <at.sisy@gmail.com>
2017-02-21 20:50:13 +0900

    とりあえずウィンドウを表示だけはできるようにした

---
+++ src/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.opencv.core.Core;
+
+/**
+ * Created by Anne on 2017/02/21.
+ */
+public class Main extends Application {
+
+    public static void main(String argv[]){
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        launch(argv);
+    }
+
+    @Override
+    public void start(Stage stage){
+        VBox root = new VBox();
+
+        // シーンの作成
+        Scene scene = new Scene(root);
+
+        // ウィンドウ表示
+        stage.setScene( scene );
+        stage.show();
+    }
+}
