d1f9302aad6fcb627d29b4e19a48366c2f913180
Anne1231 <at.sisy@gmail.com>
2017-02-21 20:59:02 +0900

    ウィンドウの初期値を設定するようにした

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,6 +11,9 @@ import org.opencv.core.Core;
  */
 public class Main extends Application {
 
+    private static final int WINDOW_WIDTH  = 1366;
+    private static final int WINDOW_HEIGHT = 768;
+
     public static void main(String argv[]){
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         launch(argv);
@@ -18,13 +21,21 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage){
+
+        /*
+        *ウィンドウの初期設定
+         */
+        stage.setTitle("Yuri Animation");
+        stage.setWidth(WINDOW_WIDTH);
+        stage.setHeight(WINDOW_HEIGHT);
+
         VBox root = new VBox();
 
         // シーンの作成
         Scene scene = new Scene(root);
 
         // ウィンドウ表示
-        stage.setScene( scene );
+        stage.setScene(scene);
         stage.show();
     }
 }
