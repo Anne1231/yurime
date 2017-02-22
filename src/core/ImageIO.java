0fae03184a2b5429dbcc8109addd4a809dd5757d
Anne1231 <at.sisy@gmail.com>
2017-02-22 20:23:35 +0900

    画像の選択とオープンをセットで行う関数を定義

--- src/core/ImageIO.java
+++ src/core/ImageIO.java
@@ -2,6 +2,8 @@ package core;
 
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 
 import java.io.File;
 
@@ -16,7 +18,7 @@ public class ImageIO {
     * GetImagePath関数
     * 画像を開いて、パスを取得する関数
      */
-    public static  String GetImagePath(Stage stage) {
+    public static String GetImagePath(Stage stage) {
 
         image_selecter.setTitle("File select");
         image_selecter.getExtensionFilters().add(new FileChooser.ExtensionFilter("イメージファイル", "*.jpg", "*.png"));
@@ -25,6 +27,20 @@ public class ImageIO {
 
         if(img_file != null) {
             return img_file.getPath().toString();
+        }else{
+            return null;
+        }
+    }
+
+    /*
+    * SelectAndOpenImage関数
+    * 画像を選択し、開く処理を一度にこなす関数
+     */
+    public static Mat SelectAndOpenImage(Stage stage){
+        String path = GetImagePath(stage);
+        if(!path.isEmpty()){
+            Mat img = Imgcodecs.imread(path);
+            return img;
         }else{
             return null;
         }
