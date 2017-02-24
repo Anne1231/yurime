c8d94294f97306f6b1d36b32be66dbb0fdece008
Anne1231 <at.sisy@gmail.com>
2017-02-24 11:23:50 +0900

    画像を移動させる動画を吐く関数を定義

--- src/core/BasicOpe.java
+++ src/core/BasicOpe.java
@@ -1,6 +1,9 @@
 package core;
 
+import javafx.scene.image.Image;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.videoio.VideoWriter;
 
 /**
  * Created by anne on 17/02/23.
@@ -21,12 +24,49 @@ public class BasicOpe {
     public static void PasteImage(Mat base, Mat image, Point2I start_point){
         byte[] color = new byte[3];
         int ix, iy = 0;
-        for(int y = start_point.getY();y < base.rows() && iy < image.rows(); y++, iy++){
+        int base_row = base.rows();
+        int image_row = image.rows();
+        int base_col = base.cols();
+        int image_col = image.cols();
+        int begin_x = start_point.getX();
+        int begin_y = start_point.getY();
+        for(int y = begin_y;y < base_row && iy < image_row; y++, iy++){
             ix = 0;
-            for(int x = start_point.getX();x < base.cols() && ix < image.cols(); x++, ix++){
+            for(int x = begin_x;x < base_col && ix < image_col; x++, ix++){
                 image.get(iy, ix, color);
                 base.put(y, x, color);
             }
         }
     }
+
+    public static void MoveImage(VideoWriter writer, Mat base, Mat move_image, Point2I start, Point2I end, int ms){
+        int distance, times;
+        double angle, step, stepx, stepy, space;
+
+        //二点間の距離
+        distance = (int)start.Distance(end);
+        //動かす間隔(ミリ秒)
+        space = ms / distance;
+        space /= (double)(1000.0 / 60.0);
+
+        //一歩の長さ
+        step = 1 / space;
+        //動かす回数
+        times = (int)(distance / step);
+        //角度
+        angle = Math.atan((end.getY() - start.getY()) / (end.getX() - start.getX()));
+        //xy
+        stepx = step * Math.cos(angle);
+        stepy = step * Math.sin(angle);
+
+        for(int i = 0;i < times;i++){
+            System.out.println(i + "/" + times);
+            Mat result = base.clone();
+            PasteImage(result, move_image, start);
+            start.addX((int)stepx);
+            start.addY((int)stepy);
+            writer.write(result);
+        }
+    }
+
 }
